a22847075c4d7827a7cf2f587f6fe7eb48d303a5
Kaiser <138375069+Kaiser131@users.noreply.github.com>
2025-07-01 22:05:36 +0600

    link changed

--- src/Hooks/Axios/useAxiosCommon.jsx
+++ src/Hooks/Axios/useAxiosCommon.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React from 'react';
 const axiosCommon = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://ph-task-back.vercel.app'
 });
 
 const useAxiosCommon = () => {
--- src/Hooks/Axios/useAxiosSecure.jsx
+++ src/Hooks/Axios/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React from 'react';
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://ph-task-back.vercel.app',
     withCredentials: true
 });
 
